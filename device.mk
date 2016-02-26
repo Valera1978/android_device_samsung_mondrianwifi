b90b90d8ca92f54a190b0c8a98357de14f601121
Valera <valera1978@tut.by>
2016-02-26 11:12:20 +0300

    Update PRODUCT_AAPT_PREF_CONFIG to xhdpi 12.2" 2560x1600 - aprox 248 dpi. It should be hdpi (240 dpi), but in stock "ro.sf.lcd_density=320" - this is xhdpi

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
