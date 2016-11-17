cc96b55cad541cbd4bba0c0a6fc923c832112849
Valera1978 <valera1978@tut.by>
2016-11-17 14:22:09 +0300

    Update to P905XXUABPE2

--- device.mk
+++ device.mk
@@ -52,7 +52,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
@@ -69,9 +68,7 @@ PRODUCT_PACKAGES += \
     gps.msm8974
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/sap.conf:/system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf
+    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf
 
 # Input device
 PRODUCT_COPY_FILES += \
