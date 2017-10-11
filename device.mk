d44454194429169542a5289b097597361a491b48
Valera1978 <valera1978@tut.by>
2017-10-11 15:37:26 +0300

    add root

--- device.mk
+++ device.mk
@@ -142,5 +142,9 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
+# Root
+PRODUCT_PACKAGES += \
+    su
+
 # common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)
