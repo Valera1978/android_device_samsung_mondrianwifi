2f1522dfe674f667a994a24ebb94bb9325a92f37
Valera1978 <valera1978@tut.by>
2019-11-30 19:57:10 +0300

    libsecnativefeature from sources

--- device.mk
+++ device.mk
@@ -93,6 +93,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Radio
+PRODUCT_PACKAGES += \
+    libsecnativefeature
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
